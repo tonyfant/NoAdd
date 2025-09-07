88a9f5d72c9ebbd07548341a1eea7887a9471722
tonyfant <entifantin@gmail.com>
2025-09-07 14:44:02 +0200

    Initial Commit

---
+++ NoAdd/NoAddApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoAddApp.swift
+//  NoAdd
+//
+//  Created by Anthony Fantin on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NoAddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
